214a481a285611d3705113a9cdcec4671b9e62e7
Scarletrose97 <99597660+Scarletrose97@users.noreply.github.com>
2022-04-01 11:55:08 +0530

    Update Calc_samikshaversion2.py

--- Calc_samikshaversion2.py
+++ Calc_samikshaversion2.py
@@ -1,14 +1,45 @@
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
-sum = float(num1) + float(num2)
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+def add(x, y):
+   """This function adds two numbers"""
 
-sub = float(num1) - float(num2)
-print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
+   return x + y
 
-mul = float(num1) * float(num2)
-print('Product of {0} and {1} is {2}' .format(num1, num2, mul))
+def subtract(x, y):
+   """This function subtracts two numbers"""
 
-div = float(num1) / float(num2)
-print ('Division of {0} and {1} is {2}' .format(num1, num2,div))
+   return x - y
 
+def multiply(x, y):
+   """This function multiplies two numbers"""
+
+   return x * y
+
+def divide(x, y):
+   """This function divides two numbers"""
+
+   return x / y
+
+# take input from the user
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = input("Enter choice(1/2/3/4):")
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input")
