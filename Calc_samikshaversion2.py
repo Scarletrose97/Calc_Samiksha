82b28974eb501440741c6310c17da3dff406f500
Scarletrose97 <99597660+Scarletrose97@users.noreply.github.com>
2022-04-01 11:46:51 +0530

    Update Calc_samikshaversion2.py

--- Calc_samikshaversion2.py
+++ Calc_samikshaversion2.py
@@ -6,4 +6,9 @@ print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 sub = float(num1) - float(num2)
 print('The sub of {0} and {1} is {2}'.format(num1, num2, sub))
 
+mul = num1 * num2
+print('Product of {0) and {1} is {2}' .format(num1, num2, mul))
+
+div = num1 / num2
+print ('Division of {0) and {1} is {2}' .format(num1, num2,div))
 
